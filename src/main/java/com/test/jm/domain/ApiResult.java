6a7de550f3dc75702d5432af8b234d2e6e393276
linweili <295287765@qq.com>
2018-12-06 16:22:48 +0800

    apiresult

---
+++ src/main/java/com/test/jm/domain/ApiResult.java
@@ -0,0 +1,44 @@
+package com.test.jm.domain;
+
+import com.test.jm.dto.test.ApiDTO;
+
+import java.util.List;
+
+public class ApiResult {
+    private String status;
+    private String msg;
+    private List<ApiDTO> apiDTOList;
+
+    public ApiResult() {
+    }
+
+    public ApiResult(String status, String msg, List<ApiDTO> apiDTOList) {
+        this.status = status;
+        this.msg = msg;
+        this.apiDTOList = apiDTOList;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<ApiDTO> getApiDTOList() {
+        return apiDTOList;
+    }
+
+    public void setApiDTOList(List<ApiDTO> apiDTOList) {
+        this.apiDTOList = apiDTOList;
+    }
+}
